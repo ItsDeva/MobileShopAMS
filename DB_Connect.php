34fb2d177c507d5a3ba14bf4953b0df87be14a8c
Devarajan <ndevaraj424@gmail.com>
2017-10-13 22:35:37 +0530

    DBConnect fully converted into MySqQLi

--- DB_Connect.php
+++ DB_Connect.php
@@ -1,33 +1,24 @@
-<!--
 <?php
+$host="localhost";
 
-  $host="localhost";
+$user="root";
 
-  $user="root";
+$pwd="root@123";
 
-  $pwd="root@123";
-
-  $database="mobilestore";
-
-  $con=($GLOBALS["___mysqli_ston"] = mysqli_connect($host, $user, $pwd)) or die("Cannot Connect to Database");
-
-  ((bool)mysqli_query($con, "USE " . $database)) or die ("Error Occured");
-  ?>
-   -->
-   
-<?php
- $con = mysqli_connect("localhost", "root", "", "demo");
+$database="mobilestore";
+$con = mysqli_connect($host,$user,$pwd,$database);
+ //$con = mysqli_connect("localhost", "root", "", "demo");
  // Check connection
 if($con === false){
  die("ERROR: Could not connect. " . mysqli_connect_error());
  }
  // Attempt insert query execution
-$sql = "INSERT INTO table_name (id, name, email) VALUES (1, 'ajay', 'ajay.agrahari09@mail.com')";
-if(mysqli_query($con, $sql)){
- echo "Records added successfully.";
- } else{
- echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
- }
+//$sql = "INSERT INTO table_name (id, name, email) VALUES (1, 'ajay', 'ajay.agrahari09@mail.com')";
+// if(mysqli_query($con, $sql)){
+//  echo "Records added successfully.";
+//  } else{
+//  echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+//  }
  // Close connection
  mysqli_close($con);
 ?>
