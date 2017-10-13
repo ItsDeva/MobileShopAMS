23b8f7139ead45a5ae13f4361de3e0a572b1b58b
Devarajan <ndevaraj424@gmail.com>
2017-10-14 02:05:03 +0530

    Total Connection Files Changed

--- DB_Connect.php
+++ DB_Connect.php
@@ -6,19 +6,13 @@ $user="root";
 $pwd="root@123";
 
 $database="mobilestore";
-$con = mysqli_connect($host,$user,$pwd,$database);
+$conn = mysqli_connect($host,$user,$pwd,$database);
  //$con = mysqli_connect("localhost", "root", "", "demo");
  // Check connection
-if($con === false){
+if($conn === false){
  die("ERROR: Could not connect. " . mysqli_connect_error());
  }
- // Attempt insert query execution
-//$sql = "INSERT INTO table_name (id, name, email) VALUES (1, 'ajay', 'ajay.agrahari09@mail.com')";
-// if(mysqli_query($con, $sql)){
-//  echo "Records added successfully.";
-//  } else{
-//  echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
-//  }
+ //
  // Close connection
- mysqli_close($con);
+// mysqli_close($con);
 ?>
