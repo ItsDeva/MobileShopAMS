2d1f0f7b87e0d26036a781c2d965ac6b5c2a6617
Devarajan <ndevaraj424@gmail.com>
2017-10-13 07:57:55 +0530

    Updated DB_Connect, Valus to be changed according to the database

--- DB_Connect.php
+++ DB_Connect.php
@@ -1,4 +1,4 @@
-
+<!--
 <?php
 
   $host="localhost";
@@ -12,6 +12,22 @@
   $con=($GLOBALS["___mysqli_ston"] = mysqli_connect($host, $user, $pwd)) or die("Cannot Connect to Database");
 
   ((bool)mysqli_query($con, "USE " . $database)) or die ("Error Occured");
+  ?>
+   -->
+   
+<?php
+ $con = mysqli_connect("localhost", "root", "", "demo");
+ // Check connection
+if($con === false){
+ die("ERROR: Could not connect. " . mysqli_connect_error());
+ }
+ // Attempt insert query execution
+$sql = "INSERT INTO table_name (id, name, email) VALUES (1, 'ajay', 'ajay.agrahari09@mail.com')";
+if(mysqli_query($con, $sql)){
+ echo "Records added successfully.";
+ } else{
+ echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+ }
+ // Close connection
+ mysqli_close($con);
 ?>
-
-//Old One
