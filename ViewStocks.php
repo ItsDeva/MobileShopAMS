a3f81c542447119b0a54991805b3207419ea3c9b
Devarajan <ndevaraj424@gmail.com>
2017-10-14 03:00:33 +0530

    Added some css for good REndering , but need to improve the View

--- ViewStocks.php
+++ ViewStocks.php
@@ -9,11 +9,11 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="livesearch.js"></script>
   <script src="styles/selectrow.js" type="javascript"></script>
-
-  <link href="styles/selectrow.css" rel="stylesheet"></link>
-
-
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+
+
+  <link href="selectrow.css" rel="stylesheet"></link>
 </head>
 <body>
   <div class="container">
