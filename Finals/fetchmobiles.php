7f143083857e4bb60c18293f59cfb30f3f1451ac
Devarajan <ndevaraj424@gmail.com>
2017-10-14 23:18:24 +0530

    edited fetch mobiles

--- Finals/fetchmobiles.php
+++ Finals/fetchmobiles.php
@@ -1,27 +1,30 @@
 <?php
 //fetch.php
 require 'Util/DB_Connect.php';
-$output = '';
-if(isset($_POST["query"]))
-{
- $search = mysqli_real_escape_string($connect, $_POST["query"]);
+$output .= '
+ <div class="table-responsive">
+  <table class="table table bordered" id="example">
+   <tr>
+   <th>Brand</th>
+   <th>Model</th>
+   <th>IMEI1</th>
+    <th>IMEI2</th>
+     <th>INDATE</th>
+     <th>DPRATE</th>
 
-$sql = "SELECT brand,model,count(brand) as stocks FROM `mobiles` group by model having brand like '%".$search."%'";
-}
-else
-{
- $sql = "SELECT brand,model,count(brand) as stocks FROM `mobiles` group by model";
-}
-$result = mysqli_query($connect, $sql);
-if(mysqli_num_rows($result) > 0)
+   </tr>
+';
+while($row = mysqli_fetch_array($result))
 {
  $output .= '
-  <div class="table-responsive">
-   <table class="table table bordered" id="example">
-    <tr>
-    <th>Brand</th>
-    <th>Model</th>
-    <th>stocks</th>
+  <tr>
+   <td><a href="viewmobile.php?id='.$row["brand"].'">'.$row["brand"].'</a></td>
+   <td>'.$row["model"].'</td>
+   <td>'.$row["imei1"].'</td>
+   <td>'.$row["IMEI2"].'</td>
+   <td>'.$row["indate"].'</td>
+   <td>'.$row["dprate"].'</td>
+
 
     </tr>
  ';
